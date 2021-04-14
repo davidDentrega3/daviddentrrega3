b6f2d3ca661ed822976c0bb6219f6e63a37fae87
davidDentrega3 <82503465+davidDentrega3@users.noreply.github.com>
2021-04-14 04:05:51 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,15 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBFda6XTyRClXw1jbql6Zw2UWCiRSbimCw",
+    authDomain: "daviddentrega3-35dce.firebaseapp.com",
+    projectId: "daviddentrega3-35dce",
+    storageBucket: "daviddentrega3-35dce.appspot.com",
+    messagingSenderId: "294728383743",
+    appId: "1:294728383743:web:b14614654b94d82ef220ca",
+    measurementId: "G-24GYFK1P7Z"
+});
+
+
+   
+    
+  
